cf6cec36b8f12271cdd0d29301bc8f1ea7415795
siyonuprardhanamandhiram <siyonuprardhanamandir@gmail.com>
2026-01-19 23:44:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // --- 1. INCREMENT THIS VERSION NUMBER EVERY TIME YOU UPDATE CODE ---
-const CACHE_NAME = "siyonu-app-v2"; 
+const CACHE_NAME = "siyonu-app-v2.1"; 
 
 const ASSETS_TO_CACHE = [
   "/",
